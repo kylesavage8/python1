9c793b514e20f494ada6d014f4c3a36218eb856d
kylesavage8 <khadfield8@yahoo.com>
2021-10-23 19:32:23 -0400

    created a random function

--- main.py
+++ main.py
@@ -94,21 +94,7 @@ def coin_flip():
 # Programming Assignment 5
 # Branching and Modules
 
-def today():
-    # Make tday = datetime.date.today
-    tday = datetime.date.today()
-    # Make timedelta  days=7
-    tdelta = datetime.timedelta(days=7)
-    # Print tday + tdelta that shows 7 days from today's date
-    print(tday + tdelta)
 
-def os_module():
-    # Print to see what the current directory is
-    print(os.getcwd())
-    # Print to see what files are on the directory
-    print(os.listdir())
-    # Print the size of venv file which displays the byte size
-    print(os.stat('venv').st_size)
 
 def random_module():
     # Displays a range of a deck of cards from 1 to 50
@@ -120,8 +106,8 @@ def random_module():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    today()
-    os_module()
+    #today()
+    #os_module()
     random_module()
     # text_art()
     # coin_flip()
