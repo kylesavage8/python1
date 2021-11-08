783bfd267b40662447706f7e3d3829f77386e933
kylesavage8 <khadfield8@yahoo.com>
2021-11-07 21:09:07 -0500

    This is the new regex branch

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import re
 import os
+from fileinput import filename
 
 if __name__ == '__main__':
     if os.access("regex.txt", os.F_OK):
         print("File is there.")
     # Make f_ok open the regex.txt to read the file
-    f_ok = open("regex.txt", "r")
-    getlines = f_ok.readlines()
+    f = open("regex.txt", "r")
+    getlines = f.readlines()
     # Make a for loop so le = line gets the line
     for le in getlines:
         locateitems = re.locateall(".$", le)
@@ -34,3 +35,8 @@ if __name__ == '__main__':
     if "Current computer accounts" in os.getcwd():
         print("Login name: ", re.split("\\\\", os.getcwd())[2])
         print("Current directory: ", re.split('\\\\', os.getcwd())[0])
+
+
+
+
+
