dbd74a4ab3e418901aeca8364abc30151721748b
kylesavage8 <khadfield8@yahoo.com>
2021-11-07 20:32:54 -0500

    This is the regex branch.

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import os
 import re
+import os
 
 if __name__ == '__main__':
-    if os.access("access.txt", os.F_OK):
-        print("The file exists.")
-
-    f = open("access.txt", "r")
-    getlines = f.readlines()
-
-    for line in getlines:
-        findingstuff = re.findall(".$", line)
-        for i in findingstuff:
+    if os.access("regex.txt", os.F_OK):
+        print("File is there.")
+    # Make f_ok open the regex.txt to read the file
+    f_ok = open("regex.txt", "r")
+    getlines = f_ok.readlines()
+    # Make a for loop so le = line gets the line
+    for le in getlines:
+        locateitems = re.locateall(".$", le)
+        # Make a nested for loop so i = "%"
+        for i in locateitems:
             if i == "%":
-                print(f"Line: {line}")
-                print(f"The last character is {i} in the sentence above.")
-
-    if os.access("access.txt", os.F_OK):
-        print("The file access.txt exists.")
+                # Print the line
+                print(f"le: {le}")
+                # Print the final character in the previous line
+                print(f"Final character is {i} in the previous print line")
+    # Make a if statement to check if the regex.txt file is still there
+    if os.access("regex.txt", os.F_OK):
+        print("File regex.txt is there.")
     try:
-        os.renames("access.txt", "new_access.txt")
-        print("Changed access.txt to new_access.txt")
+        # Rename the text file to something else
+        os.rename("regex.txt", "new_regex.txt")
+        # Print the new text file
+        print("Renamed regex.txt -> new_regex.txt")
         if os.access("access.txt", os.F_OK):
-            print("The file access.txt no longer exists.")
+            # Print to check if the original text file was properly removed
+            print("File regex.txt was removed.")
     except:
         pass
-    if "Users" in os.getcwd():
-        print("Username is: ", re.split("\\\\", os.getcwd())[2])
-    print("Drive is: ", re.split('\\\\', os.getcwd())[0])
-
-
-
+    # Make an if statement to return the absolute path of the working directory
+    if "Current computer accounts" in os.getcwd():
+        print("Login name: ", re.split("\\\\", os.getcwd())[2])
+        print("Current directory: ", re.split('\\\\', os.getcwd())[0])
